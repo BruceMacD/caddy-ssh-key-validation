0dadd247c81b72c4d95a6c584df61e4ab80a5e6b
Bruce MacDonald <brucewmacdonald@gmail.com>
2023-04-11 17:14:43 -0400

    output cert info

--- keypair.go
+++ keypair.go
@@ -3,8 +3,10 @@ package keypair
 import (
 	"context"
 	"encoding/base64"
+	"encoding/pem"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
@@ -48,17 +50,17 @@ func (m *KeypairMiddleware) Provision(ctx caddy.Context) error {
 
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		panic(err.Error())
+		return err
 	}
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		panic(err.Error())
+		return err
 	}
 	namespace := "default"
 	name := "keypair-user-mapping"
 	userKeys, err := clientset.CoreV1().ConfigMaps(namespace).Get(context.Background(), name, metav1.GetOptions{})
 	if err != nil {
-		panic(err.Error())
+		return err
 	}
 
 	userMapping := make(map[string]string)
@@ -68,6 +70,25 @@ func (m *KeypairMiddleware) Provision(ctx caddy.Context) error {
 	}
 
 	m.userMapping = userMapping
+
+	// Read the certificate file
+	certPath := "/data/caddy/certificates/local/localhost/localhost.crt"
+	certBytes, err := ioutil.ReadFile(certPath)
+	if err != nil {
+		return err
+	}
+
+	// Check if the file is a valid PEM-encoded certificate
+	block, _ := pem.Decode(certBytes)
+	if block == nil || block.Type != "CERTIFICATE" {
+		return fmt.Errorf("invalid PEM-encoded certificate")
+	}
+
+	// Encode the certificate to base64 for use in a kube config file
+	base64Cert := base64.StdEncoding.EncodeToString(pem.EncodeToMemory(block))
+
+	// Output the certificate in the expected format
+	m.logger.Info("certificate authority", zap.String("certificate", base64Cert))
 
 	return nil
 }
